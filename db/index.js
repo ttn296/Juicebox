7bbdc678905b04183766f119ffdf942cb1cc1f6f
Thuy Trang Nguyen <thuytrangnguyen@MacBook-Pro.local>
2023-01-12 15:23:59 -0500

    wrapping up part 1

--- db/index.js
+++ db/index.js
@@ -2,11 +2,23 @@ const { Client } = require('pg');
 
 const client = new Client('postgres://localhost:5432/juicebox-dev');
 
+async function createUser({ username, password }) {
+  try {
+    const { rows } = await client.query(`
+      INSERT INTO users(username, password) 
+      VALUES($1, $2) 
+      ON CONFLICT (username) DO NOTHING 
+      RETURNING *;
+    `, [username, password]);
+
+    return rows;
+  } catch (error) {
+    throw error;
+  }
+}
+
 async function getAllUsers() {
-  const { rows } = await client.query(
-    `SELECT id, username 
-    FROM users;
-  `);
+  const { rows } = await client.query(`SELECT id, username FROM users;`);
 
   return rows;
 }
@@ -14,4 +26,5 @@ async function getAllUsers() {
 module.exports = {
     client,
     getAllUsers,
+    createUser,
   }
--- db/seed.js
+++ db/seed.js
@@ -1,37 +1,62 @@
-const { client, getAllUsers } = require('./index');
+const { client, getAllUsers, createUser } = require('./index');
 
 
 async function testDB() {
     try {
-        const result = await client.query(`SELECT * FROM users;`);
-        console.log(result);
-        const users = await getAllUsers();
-        console.log(users)
+      console.log("Starting to test database...");
+
+      const users = await getAllUsers();
+      console.log("getAllUsers:", users);
+  
+      console.log("Finished database tests!");
     } catch (error) {
-      console.error(error);
-    } finally {
-        client.end();
+      console.error("Error testing database!");
+      throw error;
     }
   }
   
-  
   async function dropTables() {
     try {
+      console.log("Starting to drop tables...");
        await client.query(`
-    
+       DROP TABLE IF EXISTS users;
       `);
+
+      console.log("Finished dropping tables!");
     } catch (error) {
-      console.error(error)
+      console.error("Error dropping tables!");
       throw error;
     }
   }
 
   async function createTables() {
     try {
+      console.log("Starting to build tables...");
+
       await client.query(`
-  
+      CREATE TABLE users (
+        id SERIAL PRIMARY KEY,
+        username varchar(255) UNIQUE NOT NULL,
+        password varchar(255) NOT NULL
+      );
       `);
+      console.log("Finished building tables!");
     } catch (error) {
+      console.error("Error building tables!");
+      throw error;
+    }
+  }
+  async function createInitialUsers() {
+    try {
+      console.log("Starting to create users...");
+  
+    await createUser({ username: 'albert', password: 'bertie99' });
+    await createUser({ username: 'sandra', password: '2sandy4me' });
+    await createUser({ username: 'glamgal', password: 'soglam' });
+  
+      console.log("Finished creating users!");
+    } catch(error) {
+      console.error("Error creating users!");
       throw error;
     }
   }
@@ -42,12 +67,14 @@ async function testDB() {
   
       await dropTables();
       await createTables();
+      await createInitialUsers();
     } catch (error) {
       console.error(error);
-    } finally {
-      client.end();
-    }
+    } 
   }
-  
-  rebuildDB();
+
+  rebuildDB()
+  .then(testDB)
+  .catch(console.error)
+  .finally(() => client.end());
  
